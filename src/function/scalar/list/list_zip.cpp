f5a8b731332f60ed154623374080800b0c50cb0f
fivetran-kwoodbeck <kris.woodbeck@fivetran.com>
2026-05-13 16:37:37 -0400

    fix: list_zip segfault with empty/null arg of nested element type

--- src/function/scalar/list/list_zip.cpp
+++ src/function/scalar/list/list_zip.cpp
@@ -111,13 +111,19 @@ static void ListZipFunction(DataChunk &args, ExpressionState &state, Vector &res
 		result_data[j].offset = offset;
 		offset += len;
 	}
-	for (idx_t child_idx = 0; child_idx < args_size; child_idx++) {
-		if (args.data[child_idx].GetType() != LogicalType::SQLNULL) {
-			struct_entries[child_idx]->Slice(ListVector::GetEntry(args.data[child_idx]), selections[child_idx],
-			                                 result_size);
+	if (result_size > 0) {
+		for (idx_t child_idx = 0; child_idx < args_size; child_idx++) {
+			if (args.data[child_idx].GetType() == LogicalType::SQLNULL ||
+			    ListVector::GetListSize(args.data[child_idx]) == 0) {
+				struct_entries[child_idx]->SetVectorType(VectorType::CONSTANT_VECTOR);
+				ConstantVector::SetNull(*struct_entries[child_idx], true);
+			} else {
+				struct_entries[child_idx]->Slice(ListVector::GetEntry(args.data[child_idx]), selections[child_idx],
+				                                 result_size);
+			}
+			struct_entries[child_idx]->Flatten(result_size);
+			FlatVector::SetValidity(*struct_entries[child_idx], masks[child_idx]);
 		}
-		struct_entries[child_idx]->Flatten(result_size);
-		FlatVector::SetValidity((*struct_entries[child_idx]), masks[child_idx]);
 	}
 	result.SetVectorType(args.AllConstant() ? VectorType::CONSTANT_VECTOR : VectorType::FLAT_VECTOR);
 }
